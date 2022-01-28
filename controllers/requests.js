9824eb44a665d9ccbf6be1728501b0891e20ad2e
furqan72672 <furqan72672@gmail.com>
2022-01-28 19:35:44 +0500

    same

--- controllers/requests.js
+++ controllers/requests.js
@@ -8,7 +8,7 @@ const Request = require('../models/request')
 
 exports.addRequest = (req, res, next) => {
     Product.find({ barcode: req.body.product }).exec().then(docs => {
-        if (docs.length === 0) return res.status(404).json({ Error: `No Product found against barcode ${req.params.barcode}` })
+        if (docs.length === 0) return res.status(404).json({ Error: `No Product found against barcode ${req.body.product}` })
         const request = new Request({
             _id: mongoose.Types.ObjectId(),
             product: docs[0]._id,
